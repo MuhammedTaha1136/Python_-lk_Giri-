2453a38250444c341ebefbb7e669d814a8f2c398
Muhammed Taha Kösem <taha.36kosem@gmail.com>
2022-03-02 22:52:18 +0300

    Update Python_Temel_Konular.py

--- Python_Temel_Konular.py
+++ Python_Temel_Konular.py
@@ -6,16 +6,29 @@ print("Merhaba")
 # Şimdide bir değişken oluşturalım ve bu değişken ile "merhaba" mesajını verelim.
 a = "Merhaba"
 print(a)
+# Burada a değişkenini oluşturup içine "Merhaba" metnini ekledik ve print komutu ile mesaj verdirttik.
 
 # Değişken oluştuturken tam sayı, metinsel gibi bazı değişken türleri vardır. Bu değişken türleri oluşturulurken
 # şöyle yazılır ;
 int # tam sayı
 str # metinsel
+# Bir değişken ile örenek verelim.
+a = int(5)
+b = str("Merhaba")
 
 # Python da kod yazarken kullanıcıdan sayıları veya komutları almak için şöyle bir yöntem kullanılır;
 a = int(input("bir sayı giriniz :")) 
-b = str(input("adınızı giriniz :"))
+b = str(input("bir metin giriniz :"))
 # İNPUT komutu sayesinde kullanıcıdan bilgileri alırız.
+
+# Biraz örnek kkomutlar verelim.
+isim =("Python")
+print(isim)
+
+sayi = int(input("sayı giriniz :"))
+isim = str(input("isim giriniz :"))
+print(sayi , isim)  # Bu ksımda print komutuyla mesaj verirken iki değişkeni virgül(,) sayesinde aynı anda mesaj verebiliyoruz.
+
 
 
 
