ec79a0261b8eb4d4744f4203a4972bb3ac4a72bc
Muhammed Taha Kösem <97670352+MuhammedTaha1136@users.noreply.github.com>
2022-03-02 22:31:03 +0300

    Add files via upload
    Python diline yeni başlamış olanlar için faydalı olabilecek bir kaç komut.

---
+++ Python_Temel_Konular.py
@@ -0,0 +1,22 @@
+# Print ("......") komutu ile istediğiniz gibi mesaj verebilirsiniz.
+print("Merhaba")
+
+# Komutu çalıştırmak için f5 tuşuna basabilirsiniz.
+
+# Şimdide bir değişken oluşturalım ve bu değişken ile "merhaba" mesajını verelim.
+a = "Merhaba"
+print(a)
+
+# Değişken oluştuturken tam sayı, metinsel gibi bazı değişken türleri vardır. Bu değişken türleri oluşturulurken
+# şöyle yazılır ;
+int # tam sayı
+str # metinsel
+
+# Python da kod yazarken kullanıcıdan sayıları veya komutları almak için şöyle bir yöntem kullanılır;
+a = int(input("bir sayı giriniz :")) 
+b = str(input("adınızı giriniz :"))
+# İNPUT komutu sayesinde kullanıcıdan bilgileri alırız.
+
+
+
+ 
